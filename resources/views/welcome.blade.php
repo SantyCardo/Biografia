fa2424ba5660b75568ffd9edd1ebf6caa37cf387
SantyCardo <159256862+SantyCardo@users.noreply.github.com>
2025-09-17 22:23:04 -0500

    Mi nombre

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biografía</title>
+</head>
+<body>
+    <h1> Santiago Cardona Prada </h1>
+</body>
+</html>
