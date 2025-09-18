484b3420f24489914475390d48f0727f23543f9d
SantyCardo <159256862+SantyCardo@users.noreply.github.com>
2025-09-17 22:29:48 -0500

    Mi procedencia

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1> Santiago Cardona Prada </h1>
+    <p> Naci en Medellin, el 31 de enero de 2004, soy hijo de una Madre Santandereana (Marly Sullay Prada) y un Padre Antioqueño (Fernando Uriel Cardona Rave), tengo 3 hermanos, un hombre, llamado Samuel por parte de mi madre y mi padrastro, y dos niñas, Maria Paula tambien por parte de mi mamá y Emilia Cardona Restrepo.com (Como a ella le gusta ser llamada) por parte de mi papá, ellos son mi razón en esta vida.</p>
 </body>
 </html>
